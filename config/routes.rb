3bae2d04d7b6dfbc24771444b57baca0f0098165
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-17 10:34:53 +0000

    1) Adding omniauth to allow facebook sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
